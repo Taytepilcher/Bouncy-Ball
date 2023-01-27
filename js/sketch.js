c0861ca765aa0401f03031943bcae3e9dfd376c8
Taytepilcher <91669192+Taytepilcher@users.noreply.github.com>
2023-01-27 13:55:12 +0000

    Initial commit

---
+++ js/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    var canvas = createCanvas(800, 800);
+    canvas.parent("p5container");
+}
+
+function draw() {
+    background(220);
+
+    fill(255, 255, 255);
+    ellipse(400, 400, 200);
+}
